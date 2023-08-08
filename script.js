7b4f647d3d9e84272e129e0c50f8dc9c3277f12e
nikkikim <dasulkim92@g.ucla.edu>
2023-08-08 11:16:43 +0900

    a
    a

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-// function toggleMenu() {
+function toggleMenu() {
 
-//     let x = document.getElementById("links");
-//     if (x.style.display === "block") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "block";
-//     }
+    let x = document.getElementById("links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 
-//     let m = document.getElementById("bars");
-//     if (m.src.endsWith("images/menu.png")) {
-//         m.src = "images/cross.png";
-//     } else {
-//         m.src = "images/menu.png";
-//     }
-// }
-// var isMobile = false;
+    let m = document.getElementById("bars");
+    if (m.src.endsWith("images/menu.png")) {
+        m.src = "images/cross.png";
+    } else {
+        m.src = "images/menu.png";
+    }
+}
+
 // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-//     isMobile = true;
-//     document.write(".");
+
+//     document.write("mobile device");
 //     console.log("mobile");
 // }else{
-//     isMobile = false;
 
-//     document.writeln("<style>body{margin:0;}</style><div style='width:100%;height:100%;background-color:white;'><img style='width:1000;position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);' src='ar.png'></div>");
-
+//     document.writeln("<div style='position:fixed;width:100%;height:100%;top:150px;left:0px;background-color:black;'><img style='width:50%;margin:0 auto;display:block;' src='ma.jpg'></div>");
+//     window.stop();
 // }
 
-// window.onload = function (){
-//     if (!isMobile){
-//         document.getElementById('content2').style.display = 'none';
-//     } else {
-//         document.getElementById('content2').style.display = 'block';
-//     }
-  
-// };
-// function videoHandler(e) {
-//     // What you want to do after the event
-//     document.getElementById('content2').style.display = 'block';
-//     document.getElementById('video2').style.display = 'none';
-// }
+window.onload = function (){
+    if (document.getElementById('video2') != undefined){
+        document.getElementById('video2').addEventListener('ended', videoHandler, false);
+    } else {
+        document.getElementById('content2').style.display = 'block';
+        console.log("No video");
+    }
+};
+function videoHandler(e) {
+    // What you want to do after the event
+    document.getElementById('content2').style.display = 'block';
+    document.getElementById('video2').style.display = 'none';
+}
